ce8129bcf5c1fc83064ff8c6b2a4ab522f672ccc
miftah-mj <jannat27120@gmail.com>
2025-02-08 18:45:23 +0600

    add layout components and update homepage; include Navbar and Footer

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Root = () => {
     return (
-        <div>
-            root
-            <Outlet />
+        <div className="flex flex-col min-h-screen">
+            {/* Navbar */}
+            <Navbar />
+
+            {/* Main Content */}
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+
+            {/* Footer */}
+            <Footer />
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router";
+
 const Home = () => {
-    return <div>home</div>;
+    return (
+        <div style={{ textAlign: "center", marginTop: "50px" }}>
+            <h1>Welcome to the Quiz App</h1>
+            <Link to="/quiz" className="btn btn-soft btn-primary mt-3">
+                Start Quiz
+            </Link>
+        </div>
+    );
 };
 
 export default Home;
